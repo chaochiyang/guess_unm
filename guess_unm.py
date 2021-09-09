a1a0fc70bb5343aeb336ac27ae6b5d9030253909
Simon <chaochiyang@gmail.com>
2021-09-09 15:12:19 +0800

    add count

--- guess_unm.py
+++ guess_unm.py
@@ -1,13 +1,17 @@
 import random
 
 r = random.randint(1,100)
+count = 0
 while True:
+	count += 1 # count = count + 1
 	ans = input('請在1~100間猜一個數字:')	
 	ans = int(ans)
 	if ans == r:
 		print('你猜中了！')
+		print('這是你猜的', count, '次')
 		break
 	elif ans > r:
-		print('你猜的比正確答案大')
+		print('比答案大')
 	elif ans < r:
-		print('你猜的比正確答案小')
+		print('比答案小')
+	print('這是你猜的', count, '次')
